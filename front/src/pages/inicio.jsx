f701d4cd309a5ac06cd38f88a6c1bc4e81b3d01c
Marte <giannoncelli_giuliano@hotmail.com>
2024-10-04 13:53:21 -0300

    Cambio de color de inicio

--- front/src/pages/inicio.jsx
+++ front/src/pages/inicio.jsx
@@ -7,7 +7,7 @@ const Inicio = () => {
     return (
         <div className="inicio">
             <div className="header">
-                <h2>Demo de aplicacion web para Red de monitoreo de la Cuenca Inferior del Río Chubut.</h2>
+                <h1>Demo de aplicacion web para Red de monitoreo de la Cuenca Inferior del Río Chubut.</h1>
                 <div className="images">
                     <img src={logoUNPSJB} alt="Imagen 2" />
                 </div>
