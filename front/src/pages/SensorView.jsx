6f0a00a3038936dedcad1ea99fd5b80d720510da
GonzaloAgu <gonzalo.ag88@gmail.com>
2024-10-09 21:57:56 -0300

    card con ultimo dato

--- front/src/pages/SensorView.jsx
+++ front/src/pages/SensorView.jsx
@@ -19,16 +19,21 @@ const SensorView = () => {
                     </div>
                     <button id="btn-modificar" className="btn btn-secondary">Modificar Nodo</button>
                 </div>
-                <div id="content" className="d-flex justify-content-between">
+                <div id="content" className="d-flex justify-content-between align-items-center">
                     <div id="mapa" style={{ height: '200px', width: '290px', "background-color": 'lightblue' }}></div>
                     <div id="card-container">
-                        <div className="card" style={{width: '18rem'}}>
+                        <div className="card">
                             <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" className="card-link">Card link</a>
-                                <a href="#" className="card-link">Another link</a>
+                                <h5 className="card-title">Último dato</h5>
+                                    <p className="card-text fs-5">
+                                        <div>
+                                            <i className="fa fa-thermometer me-2" aria-hidden="true"/>12.4ºC
+                                        </div>
+                                        <div>
+                                            <i className="fa fa-tint me-2" aria-hidden="true"/>1.3mts
+                                        </div>
+                                    </p>
+                                <h6 className="card-subtitle mb-2 text-body-secondary">Hace 12 minutos</h6>
                             </div>
                         </div>
                     </div>
