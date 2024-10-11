a3c4fc38e645bce2a518a9ad8cd368690d9bbbc0
GonzaloAgu <gonzalo.ag88@gmail.com>
2024-10-10 22:41:51 -0300

    grafico incorporado

--- front/src/pages/SensorView.jsx
+++ front/src/pages/SensorView.jsx
@@ -1,11 +1,14 @@
-import { Button } from "bootstrap";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../assets/font-awesome/css/font-awesome.min.css";
 import { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
+import GraphDoble from '../components/GraphDoble'
+import { randomDataForDoubleChart } from "../utils-graphs";
+
 const api = import.meta.env.VITE_API_URL;
 
-const CARD_HEIGHT = 150;
+const CARD_HEIGHT = 200;
 
 const SensorView = () => {
     const { id } = useParams();
@@ -22,58 +25,75 @@ const SensorView = () => {
                     <button id="btn-modificar" className="btn btn-secondary align-self-start">Modificar Nodo</button>
                 </div>
                 <div id="content" className="d-flex justify-content-between align-items-start">
-                   
-                    <div id="card-container" className="d-flex" style={{height: CARD_HEIGHT}}>
+
+                    <div id="card-container" className="d-flex align-items-center" style={{ height: CARD_HEIGHT }}>
                         <div className="card me-5 p-3">
-                            <div className="card-body">
+                            <div className="card-body align-items-center">
                                 <p className="card-text fs-3 d-flex">
                                     <div className="me-4">
-                                        <i className="fa fa-thermometer me-2" aria-hidden="true" />12.4ºC
+                                        <i className="fa fa-tint me-2" aria-hidden="true" />1.3m
                                     </div>
                                     <div>
-                                        <i className="fa fa-tint me-2" aria-hidden="true" />1.3mts
+                                        <i className="fa fa-thermometer me-2" aria-hidden="true" />12.4ºC
                                     </div>
                                 </p>
                                 <h6 className="card-subtitle mb-2 text-body-secondary">Hace 12 minutos</h6>
                             </div>
                         </div>
-                        <div className="card align-self-start ms-5" style={{height: CARD_HEIGHT}}>
-                            <div className="card-body">
-                                <h5 className="card-title">Máximo 24hs</h5>
+                        <div className="card me-2 p-1" >
+                            <div className="card-header">
+                                <h6 className="card-title text-center">Máximo 24hs</h6>
+                            </div>
+                            <div className="card-body d-flex flex-column justify-content-center align-items-center">
                                 <p className="card-text fs-5">
                                     <div>
-                                        <i className="fa fa-tint me-2" aria-hidden="true" />3.3mts
+                                        <i className="fa fa-tint me-2" aria-hidden="true" />3.3m
                                     </div>
                                 </p>
                                 <h6 className="card-subtitle mb-2 text-body-secondary">13:02</h6>
                             </div>
                         </div>
-                        <div className="card align-self-start"  style={{height: CARD_HEIGHT}}>
-                            <div className="card-body">
-                                <h5 className="card-title">Máximo 7 días</h5>
+                        <div className="card me-2 p-1">
+                            <div className="card-header">
+                                <h6 className="card-title text-center">Máximo 7 días</h6>
+                            </div>
+                            <div className="card-body d-flex flex-column justify-content-center align-items-center">
+
                                 <p className="card-text fs-5">
                                     <div>
-                                        <i className="fa fa-tint me-2" aria-hidden="true" />4.2mts
+                                        <i className="fa fa-tint me-2" aria-hidden="true" />4.2m
                                     </div>
                                 </p>
                                 <h6 className="card-subtitle mb-2 text-body-secondary">04/10/2024 09:39</h6>
                             </div>
                         </div>
-                        <div className="card align-self-start"  style={{height: CARD_HEIGHT}}>
-                            <div className="card-body">
-                                <h5 className="card-title">Máximo mensual</h5>
+                        <div className="card p-1">
+                            <div className="card-header  text-center">
+                                <h6>Máximo mensual</h6>
+                            </div>
+                            <div className="card-body d-flex flex-column justify-content-center align-items-center">
                                 <p className="card-text fs-5">
                                     <div>
-                                        <i className="fa fa-tint me-2" aria-hidden="true" />5.0mts
+                                        <i className="fa fa-tint me-2" aria-hidden="true" />5.0m
                                     </div>
                                 </p>
                                 <h6 className="card-subtitle mb-2 text-body-secondary">04/10/2024 09:39</h6>
                             </div>
                         </div>
                     </div>
-                    <div id="mapa" className="d-none d-lg-block" style={{ height: '270px', width: '350px', "background-color": 'lightblue' }}></div>
+                    <div id="mapa" className="d-none d-lg-block" style={{ height: CARD_HEIGHT + 'px', width: '350px', "background-color": 'lightblue' }}></div>
                 </div>
-                <hr/>
+                <hr />
+            </div>
+            <div id="data-visualizer">
+                <div className="btn-group mb-4" role="group" aria-label="Basic radio toggle button group">
+                    <input type="radio" className="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked />
+                    <label className="btn btn-outline-primary" for="btnradio1">Gráfico</label>
+
+                    <input type="radio" className="btn-check" name="btnradio" id="btnradio2" autocomplete="off" />
+                    <label className="btn btn-outline-primary" for="btnradio2">Tabla</label>
+                </div>
+                <GraphDoble data={randomDataForDoubleChart()}/>
             </div>
         </div>
     );
