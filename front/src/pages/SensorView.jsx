8ae33a130fa2b0914252bdfc763160c3f4cfae19
mariovsz <vsqzma@gmail.com>
2024-10-29 20:04:49 -0300

    SensorView

--- front/src/pages/SensorView.jsx
+++ front/src/pages/SensorView.jsx
@@ -45,11 +45,12 @@ const SensorView = () => {
         setData(info.data);
         setLoading(false);
       })
-      .catch((err) =>
+      .catch((err) => {
         console.error(
           "Se produjo un error al obtener la información del nodo: " + err
-        )
-      );
+        );
+        setLoading(false);
+      });
   }, [id]);
 
   const handleViewChange = (event) => {
@@ -68,128 +69,135 @@ const SensorView = () => {
 
   return (
     <div className="container mt-5">
-      <div id="main">
-        <div id="header" className="d-flex justify-content-between pb-1">
-          <div id="info-sensor">
-            <h2 className="d-flex align-items-center">
-              <i className="fa fa-rss me-2" aria-hidden="true" />
-              {loading ? <LoadingSpinner /> : nodo.sensor.identificador}
-            </h2>
-            <p>
-              Una breve descripción del nodo irá incluida acá una vez que se
-              implemente este campo en la base de datos.
-            </p>
-            <span>
-              <i className="fa fa-map-marker me-2" aria-hidden="true" />{" "}
+      <div id="main" className="card mb-4">
+        <div className="card-body">
+          <div id="header" className="d-flex justify-content-between pb-1">
+            <div id="info-sensor">
+              <h2 className="d-flex align-items-center">
+                <i className="fa fa-rss me-2" aria-hidden="true" />
+                {loading ? <LoadingSpinner /> : nodo.sensor.identificador}
+              </h2>
+              <p>
+                Una breve descripción del nodo irá incluida acá una vez que se
+                implemente este campo en la base de datos.
+              </p>
               <span>
-                {loading ? null : (
-                  <>
-                    <b>Latitud:</b> {nodo.sensor.latitud.toFixed(5)}
-                    <b> Longitud:</b> {nodo.sensor.longitud.toFixed(5)}
-                  </>
-                )}
+                <i className="fa fa-map-marker me-2" aria-hidden="true" />{" "}
+                <span>
+                  {loading ? null : (
+                    <>
+                      <b>Latitud:</b> {nodo.sensor.latitud.toFixed(5)}
+                      <b> Longitud:</b> {nodo.sensor.longitud.toFixed(5)}
+                    </>
+                  )}
+                </span>
               </span>
-            </span>
-          </div>
-          <button
-            id="btn-modificar"
-            className="btn btn-secondary align-self-start"
-          >
-            Modificar Nodo
-          </button>
-        </div>
-        <div
-          id="content"
-          className="d-flex justify-content-between align-items-start"
-        >
-          {loading || !data?.length ? (
-            <LoadingSpinner />
-          ) : (
-            <NodoRecentDataContainer
-              data={data}
-              CARD_HEIGHT={CARD_HEIGHT}
-            ></NodoRecentDataContainer>
-          )}
-          <div
-            id="mapa"
-            className="d-none d-xl-block"
-            style={{
-              height: CARD_HEIGHT + "px",
-              width: "320px",
-              backgroundColor: "lightblue",
-            }}
-          ></div>
-        </div>
-        <hr />
-      </div>
-      <div id="data-visualizer">
-        <div className="d-flex justify-content-start">
-          <div
-            className="btn-group mb-4 me-3"
-            role="group"
-            aria-label="Basic radio toggle button group"
-          >
-            <input
-              type="radio"
-              className="btn-check"
-              name="btnradio"
-              id="graph"
-              autoComplete="off"
-              defaultChecked
-              onChange={handleViewChange}
-            />
-            <label className="btn btn-outline-primary" htmlFor="graph">
-              Gráfico
-            </label>
-
-            <input
-              type="radio"
-              className="btn-check"
-              name="btnradio"
-              id="table"
-              autoComplete="off"
-              onChange={handleViewChange}
-            />
-            <label className="btn btn-outline-primary" htmlFor="table">
-              Tabla
-            </label>
-          </div>
-          <FiltrosFetch
-            initialSensorId={id}
-            setData={setData}
-            totalItems={totalItems}
-          />
-        </div>
-        {view === "graph" ? (
-          loading ? (
-            <LoadingSpinner />
-          ) : (
-            <GraphDoble data={data} />
-          )
-        ) : (
-          view === "table" && (
-            <div>
-              <FiltrosFetch
-                initialSensorId={id}
-                setData={setData}
-                totalItems={totalItems}
-              />
-              <Paginacion
-                itemsPerPage={itemsPerPage}
-                totalItems={totalItems}
-                currentPage={currentPage}
-                onPageChange={handlePageChange}
-              />
-              {loading ? (
-                <LoadingSpinner />
-              ) : (
-                <TablaDatos items={getVisibleData()} />
-              )}
             </div>
-          )
-        )}
+            <button
+              id="btn-modificar"
+              className="btn btn-secondary align-self-start"
+            >
+              Modificar Nodo
+            </button>
+          </div>
+          <div
+            id="content"
+            className="d-flex justify-content-between align-items-start"
+          >
+            {loading || !data?.length ? (
+              <LoadingSpinner />
+            ) : (
+              <NodoRecentDataContainer data={data} CARD_HEIGHT={CARD_HEIGHT} />
+            )}
+            <div
+              id="mapa"
+              className="d-none d-xl-block"
+              style={{
+                height: CARD_HEIGHT + "px",
+                width: "320px",
+                backgroundColor: "lightblue",
+              }}
+            ></div>
+          </div>
+        </div>
+      </div>
+      <div id="data-visualizer" className="card mb-4">
+        <div className="card-body">
+          <div className="d-flex justify-content-start">
+            <div
+              className="btn-group mb-4 me-3"
+              role="group"
+              aria-label="Basic radio toggle button group"
+            >
+              <input
+                type="radio"
+                className="btn-check"
+                name="btnradio"
+                id="graph"
+                autoComplete="off"
+                defaultChecked
+                onChange={handleViewChange}
+              />
+              <label className="btn btn-outline-primary" htmlFor="graph">
+                Gráfico
+              </label>
+
+              <input
+                type="radio"
+                className="btn-check"
+                name="btnradio"
+                id="table"
+                autoComplete="off"
+                onChange={handleViewChange}
+              />
+              <label className="btn btn-outline-primary" htmlFor="table">
+                Tabla
+              </label>
+            </div>
+            <FiltrosFetch
+              initialSensorId={id}
+              setData={setData}
+              totalItems={totalItems}
+            />
+          </div>
+          {view === "graph" ? (
+            loading ? (
+              <LoadingSpinner />
+            ) : (
+              <GraphDoble data={data} />
+            )
+          ) : (
+            view === "table" && (
+              <div>
+                <FiltrosFetch
+                  initialSensorId={id}
+                  setData={setData}
+                  totalItems={totalItems}
+                />
+                <Paginacion
+                  itemsPerPage={itemsPerPage}
+                  totalItems={totalItems}
+                  currentPage={currentPage}
+                  onPageChange={handlePageChange}
+                />
+                {loading ? (
+                  <LoadingSpinner />
+                ) : (
+                  <TablaDatos items={getVisibleData()} />
+                )}
+                <Paginacion
+                  itemsPerPage={itemsPerPage}
+                  totalItems={totalItems}
+                  currentPage={currentPage}
+                  onPageChange={handlePageChange}
+                />
+              </div>
+            )
+          )}
+        </div>
       </div>
     </div>
   );
 };
+
 export default SensorView;
