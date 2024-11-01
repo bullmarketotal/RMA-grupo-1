1c85b07d4223f4d8eb1884f55785d64a3ab204a1
mariovsz <vsqzma@gmail.com>
2024-10-31 21:01:33 -0300

    del console.log

--- front/src/components/CustomTooltip.jsx
+++ front/src/components/CustomTooltip.jsx
@@ -1,17 +1,22 @@
-export default function CustomTooltip({ payload}) {
+export default function CustomTooltip({ payload }) {
   const dato = payload[0];
   let unidad = "";
-  console.log("dato", dato);
-
   if (dato?.name === "temperatura") unidad = "ºC";
-  if (dato?.name === "nivel_hidrometrico") unidad = "m"
+  if (dato?.name === "nivel_hidrometrico") unidad = "m";
 
   return (
-    <div style={{backgroundColor: "white", border: "solid 1px black", "border-radius": "black", padding: "4px"}}>
+    <div
+      style={{
+        backgroundColor: "white",
+        border: "solid 1px black",
+        "border-radius": "black",
+        padding: "4px",
+      }}
+    >
       <svg height="12" width="12" style={{ marginRight: "8px" }}>
         <circle cx="6" cy="6" r="6" fill={payload[0]?.color || "black"} />
       </svg>
-       {dato?.value.toFixed(1)} {unidad}
+      {dato?.value.toFixed(1)} {unidad}
     </div>
   );
 }
