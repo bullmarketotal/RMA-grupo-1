2b6c6be8ef0067f095e862237bab4d5f8a299070
mariovsz <vsqzma@gmail.com>
2024-11-01 08:44:05 -0300

    delay

--- front/src/components/LoadingSpinner.jsx
+++ front/src/components/LoadingSpinner.jsx
@@ -1,37 +1,47 @@
+import React, { useEffect, useState } from "react";
+
 const LoadingSpinner = () => {
+  const [showSpinner, setShowSpinner] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShowSpinner(true), 500); // Espera 500 ms
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!showSpinner) return null;
+
+  const styles = {
+    spinner: {
+      animation: "spin 1s linear infinite",
+      width: "50px",
+      height: "50px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    circle: {
+      stroke: "#007bff",
+      strokeWidth: "5",
+      fill: "none",
+      cx: "25",
+      cy: "25",
+      r: "20",
+    },
+    visuallyHidden: {
+      visuallyHidden: true,
+    },
+  };
+
   return (
     <div className="d-flex justify-content-center mt-5">
       <svg
+        style={styles.spinner}
         xmlns="http://www.w3.org/2000/svg"
-        width="50"
-        height="50"
         viewBox="0 0 50 50"
-        className="spin"
       >
-        <circle
-          cx="25"
-          cy="25"
-          r="20"
-          stroke="#007bff"
-          strokeWidth="5"
-          fill="none"
-        />
+        <circle style={styles.circle} />
       </svg>
-      <style jsx>{`
-        .spin {
-          animation: spin 1s linear infinite;
-        }
-
-        @keyframes spin {
-          0% {
-            transform: rotate(0deg);
-          }
-          100% {
-            transform: rotate(360deg);
-          }
-        }
-      `}</style>
-      <span className="visually-hidden">Cargando...</span>
+      <span className={styles.visuallyHidden}>Cargando...</span>
     </div>
   );
 };
