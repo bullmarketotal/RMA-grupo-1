18c027c2f87eafcb92e4c296c5b04959e143d3bb
mariovsz <vsqzma@gmail.com>
2024-11-04 20:04:07 -0300

    NotificationButton

--- front/src/components/atoms/NotificationButton.jsx
+++ front/src/components/atoms/NotificationButton.jsx
@@ -6,15 +6,14 @@ const NotificationButton = () => {
     <button
       onClick={() => console.log("Hacer algo ... algo...")}
       type="button"
-      className="relative rounded-full p-2 transition-all duration-200 ease-in-out bg-transparent 
-                 text-gray-600 hover:bg-gray-200 dark:hover:bg-gray-700 dark:text-gray-400 
-                 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-600"
+      className="relative rounded-full p-2 transition-all duration-200 ease-in-out
+              text-gray-600 hover:bg-gray-50 
+             dark:text-gray-400 dark:hover:bg-gray-700"
     >
-      {/* Fondo de escala y badge */}
-      <div className="absolute -top-1 -right-1 h-2.5 w-2.5 rounded-full bg-red-600 animate-pulse" />
+      <div className="absolute -top-0 -right-0 h-2.5 w-2.5 rounded-full bg-red-600 animate-pulse" />
       <FaRegBell
         aria-hidden="true"
-        className="h-6 w-6 transition-transform transform active:scale-90"
+        className="h-6 w-6 transition-transform transform active:scale-50"
       />
     </button>
   );
