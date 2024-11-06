9f9378bffe24a35c971f3801ba7fdfcfdf2b87f5
mariovsz <vsqzma@gmail.com>
2024-11-06 12:47:49 -0300

    NodoInfo

--- front/src/components/organisms/NodoInfo.jsx
+++ front/src/components/organisms/NodoInfo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Card, LoadingSpinner, MiniMap } from "../atoms";
-import { MaxLevelCard, NodoRecentDataCard, SensorHeader } from "../molecules";
+import { MaxLevelCard, NodoRecentDataCard, NodoHeader } from "../molecules";
 
 const TIMEFRAME_24H = 1000 * 60 * 60 * 24;
 const TIMEFRAME_7D = 1000 * 60 * 60 * 24 * 7;
@@ -11,7 +11,7 @@ const NodoInfo = ({ data, loading }) => {
     <Card>
       <div className="grid grid-cols-3 grid-rows-3 gap-4">
         <div className="col-span-2">
-          <SensorHeader sensor={data.sensor} />
+          <NodoHeader sensor={data.sensor} />
         </div>
         <div className="row-span-3 col-start-3 shadow-sm rounded-lg overflow-hidden w-full h-full">
           <MiniMap lat={latitud} lng={longitud} />
