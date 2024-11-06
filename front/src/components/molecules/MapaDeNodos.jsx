e990f133da1bb0855cd084bcfedc79db55a6579b
GonzaloAgu <gonzalo.ag88@gmail.com>
2024-11-05 22:17:53 -0300

    mapa con ultimos datos funcional

--- front/src/components/molecules/MapaDeNodos.jsx
+++ front/src/components/molecules/MapaDeNodos.jsx
@@ -2,7 +2,10 @@ import React, { useEffect, useState } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import useFetchSensores from "../../hooks/useFetchSensores";
+import { obtenerStringTiempoDesdeUltimoDato } from "../utils/date";
 
+const API_URL = import.meta.env.VITE_API_URL;
+const BASE_CLASSES_FOR_MARKERS = " font-mono text-black p-2 border border-gray-800 shadow-md rounded-3xl font-serif text-sm hover:text-lg w-32 hover:w-36 text-center text-center transition-all duration-300"
 
 
 export const MapaDeNodos = () => {
@@ -10,27 +13,72 @@ export const MapaDeNodos = () => {
     [-42.342, -62.145], // suroeste
     [-45.594, -71.54], // noreste
   ];
-  
-  const addNodeToMap = (nodo, map) => {
-    const newMarker = L.marker([nodo.latitud, nodo.longitud], { draggable: false, title: nodo.identificador }).addTo(map);
-  }
 
   const [map, setMap] = useState(null);
 
   
   const posicionInicial = [-43.5042843, -65.7791978];
-  const zoomInicial = 9;
+  const zoomInicial = 10;
   
   const { data, loading, error } = useFetchSensores();
 
   useEffect(() => {
-    console.log('use effect', !map, data)
     if(!map || data?.length === 0)
       return;
+
     data.forEach(nodo => {
-      console.log(nodo)
-      addNodeToMap(nodo, map)
-    })
+      const TODAY = new Date(new Date().getTime() - 3 * 60 * 60 * 1000).toISOString().substring(0, 10);
+
+      const FETCH_URL = API_URL + `/paquetes?sensor_id=${nodo.id}&start_date=${TODAY}&end_date=${TODAY}`
+      
+      fetch(FETCH_URL)
+      .then(res => res.json())
+      .then(dataNodo => {
+        let customIcon;
+
+        // Caso sin datos hoy
+        if(dataNodo.length === 0){
+          customIcon = L.divIcon({
+            className: '',
+            html: `
+            <div class="${BASE_CLASSES_FOR_MARKERS} bg-gray-400">
+              <span class="text-xs text-nowrap text-ellipsis">${nodo.identificador}</span>
+              <br/>
+              <span class="font-bold">S/D</span>
+            </div>`
+          });
+        } 
+        // Caso con datos hoy
+        else {
+
+          const lastData = dataNodo[dataNodo.length - 1];
+
+          const stringUltimoDato = obtenerStringTiempoDesdeUltimoDato(dataNodo);
+
+          customIcon = L.divIcon({
+            className: '',
+            html: `<div class="${BASE_CLASSES_FOR_MARKERS} bg-gradient-to-tr from-blue-400 to-blue-600">
+              <span class="text-xs text-nowrap text-ellipsis">${nodo.identificador}</span>
+              <br/>
+              <span class="font-bold text-base">${lastData .nivel_hidrometrico.toFixed(1)}m</span>
+              <br/>
+              <span class="text-[0.6rem] bold ">${stringUltimoDato}</span>
+            </div>`
+          });
+        }
+
+        const nodoMarker = L.marker([nodo.latitud, nodo.longitud], {
+          icon: customIcon,
+          draggable: false
+        }).addTo(map);
+
+        nodoMarker.on('click', () => {
+          window.open(`/sensor/${nodo.id}`, '_blank');
+        });
+      })
+
+    });
+
   }, [data])
 
   
@@ -60,7 +108,7 @@ export const MapaDeNodos = () => {
 
   return (
     <section>
-      <div id="map" className="m-auto h-[600px] w-4/5"></div>
+      <div id="map" className="m-auto h-[600px] w-full"></div>
     </section>
   );
 };
--- front/src/pages/inicio.jsx
+++ front/src/pages/inicio.jsx
@@ -1,20 +1,42 @@
 import React from "react";
-import logoRMA from '../../logo.png';
+import logoRMA from "../../logo.png";
 import { MapaDeNodos } from "../components/molecules/MapaDeNodos";
+import { Container } from "../components/atoms";
 
 const Inicio = () => {
   return (
-    <>
-      <div className="container mx-auto max-w-7xl pb-8 mt-4 px-4 sm:px-6 lg:px-8 transition-colors duration-300 bg-gray-100 dark:bg-gray-900">
-        <div className="header p-3 flex justify-center">
-          <img src={logoRMA} alt="Imagen 2" className="h-20 animate-spin-slow " />
-          <h1 className="text-center align-middle ml-10 h-24 text-4xl antialiased font-bold dark:text-slate-50 uppercase">
-            Red de Monitoreo de la Cuenca <br/>Inferior del Río Chubut
+    <Container>
+      <div className=" dark:bg-gray-900 dark:text-slate-50">
+        <div className="header px-11 py-5 flex">
+          <img
+            src={logoRMA}
+            alt="Imagen 2"
+            className="h-20 animate-spin-slow "
+          />
+          <h1 className="ml-10 h-24 text-4xl antialiased font-bold dark:text-slate-50 uppercase">
+            Red de Monitoreo de la Cuenca <br />
+            Inferior del Río Chubut
           </h1>
         </div>
+        <div className="px-11 text-lg">
+          <hr className="mb-4"/>
+          <p>
+            En el mapa se visualiza el <b>último dato</b> obtenido para cada nodo de la
+            red.
+          </p>
+          <p>
+          Si no hubo datos hoy, estará en gris.
+          </p>
+          <p >
+            <b>Clickea el nodo</b> para acceder a una vista detallada de su información
+            y datos.
+          </p>
+          <div className="mt-10 border-1 border-slate-400 drop-shadow-xl">
+            <MapaDeNodos />
+          </div>
+        </div>
       </div>
-      <MapaDeNodos/>
-    </>
+    </Container>
   );
 };
 
