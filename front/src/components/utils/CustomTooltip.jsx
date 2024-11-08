94d984000f0b1d7392ba70801392766892410f1a
Gonzalo Agu <gonzalo.ag88@gmail.com>
2024-11-08 15:57:40 -0300

    tooltip completo

--- front/src/components/utils/CustomTooltip.jsx
+++ front/src/components/utils/CustomTooltip.jsx
@@ -1,3 +1,5 @@
+import { dateFormatter } from "./utils-graphs";
+
 export default function CustomTooltip({ payload }) {
   const dato = payload[0];
   let unidad = "";
@@ -5,18 +7,22 @@ export default function CustomTooltip({ payload }) {
   if (dato?.name === "nivel_hidrometrico") unidad = "m";
 
   return (
-    <div
-      style={{
+    <div style={{
         backgroundColor: "white",
+        width: "fit-content",
         border: "solid 1px black",
-        borderRadius: "black",
-        padding: "4px",
-      }}
-    >
-      <svg height="12" width="12" style={{ marginRight: "8px" }}>
-        <circle cx="6" cy="6" r="6" fill={payload[0]?.color || "black"} />
-      </svg>
-      {dato?.value.toFixed(1)} {unidad}
+        borderRadius: "1rem",
+        padding: "0.5rem 0.7rem",
+    }}>
+      <div className="text-sm text-slate-600">
+        {dateFormatter(dato?.payload.date)}
+      </div>
+      <div className="flex text-center">
+        <svg height="12" width="12" style={{ margin: "0.35rem" }}>
+          <circle cx="6" cy="6" r="6" fill={payload[0]?.color || "black"} />
+        </svg>
+        {dato?.value.toFixed(1)} {unidad}
+      </div>
     </div>
   );
 }
