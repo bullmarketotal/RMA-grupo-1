a395ca103c63518ad0cf1087b18bd548a78a35d0
GonzaloAgu <gonzalo.ag88@gmail.com>
2024-11-23 13:58:13 -0300

    unidades en tooltip

--- front/src/components/utils/CustomTooltip.jsx
+++ front/src/components/utils/CustomTooltip.jsx
@@ -3,8 +3,10 @@ import { dateFormatter } from "./utils-graphs";
 export default function CustomTooltip({ payload }) {
   const dato = payload[0];
   let unidad = "";
-  if (dato?.name === "temperatura") unidad = "ºC";
-  if (dato?.name === "nivel_hidrometrico") unidad = "m";
+  if (dato?.payload.type_id === 1) unidad = "ºC";
+  if (dato?.payload.type_id === 14) unidad = "mm";
+  if (dato?.payload.type_id === 16) unidad = "V";
+  if (dato?.payload.type_id === 25) unidad = "cm";
 
   return (
     <div className="bg-slate-100 dark:bg-slate-800 dark:text-white drop-shadow shadow-md px-3 py-2 rounded-md">
